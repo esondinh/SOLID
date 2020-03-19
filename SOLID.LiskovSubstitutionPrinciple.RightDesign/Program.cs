4482721a5542d6d5c110c13e3a50bfb384544c67
esondinh <esondinh@netstars.vn>
2020-03-20 02:34:43 +0700

    commit solid

---
+++ SOLID.LiskovSubstitutionPrinciple.RightDesign/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SOLID.LiskovSubstitutionPrinciple.RightDesign
+{
+    public interface IEmployee
+    {
+        double CalculateSalary();
+    }
+
+    public class Staff : IEmployee
+    {
+        public double CalculateSalary()
+        {
+            return 10.0;
+        }
+    }
+
+    public class Manager : IEmployee
+    {
+        public double CalculateSalary()
+        {
+            return 20.0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmployee staff = new Staff();
+            IEmployee manager = new Manager();
+
+            // staff và manager cùng thuộc một kiểu IEmployee
+            // nên có thể được sử dụng như một IEmployee
+            Console.WriteLine("Salary of staff = " + staff.CalculateSalary());
+            Console.WriteLine("Salary of manager = " + manager.CalculateSalary());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SOLID.LiskovSubstitutionPrinciple.WrongDesign/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SOLID.LiskovSubstitutionPrinciple.WrongDesign
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int CalculateSquare()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public int Side { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Square();
+            rectangle.Height = 5;
+            rectangle.Width = 10;
+
+            // Nếu Square kế thừa từ Rectangle, thì Square phải là một kiểu của Rectangle.
+            // Khi thay thế Rectangle bằng class Square sẽ cho kết quả sai nếu muốn tính toán diện tích hình vuông.
+            // Vì thế Square không thể thay thế Rectangle trong trường hợp này. 
+            // Thiết kế này vi phạm nguyên tắc LSP.
+            Console.WriteLine("Square = " + rectangle.CalculateSquare());
+            Console.ReadLine();
+        }
+    }
+}
